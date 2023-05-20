f6f49bf126e4f8dbebaef80796eabf4d47d8d5c2
Petrakov Oleg <murami.ike@gmail.com>
2023-05-21 00:03:42 +0300

    Process FB Events and save to DynamoDB

--- src/config.py
+++ src/config.py
@@ -3,3 +3,8 @@ import pydantic
 class Config(pydantic.BaseSettings):
     WEBHOOK_VERIFICATION_TOKEN: str
     FACEBOOK_CLIENT_SECRET: str
+     # YDB: via cli — ydb config profile get db1
+    DOCUMENT_API_ENDPOINT: str = "https://example.com/path/to/your/db"
+    REGION_NAME: str = "us-east-1"
+    AWS_ACCESS_KEY_ID: str = "<key-id>"
+    AWS_SECRET_ACCESS_KEY: str = "<secret-access-key>"
